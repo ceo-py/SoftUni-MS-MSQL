f54491fcfcaa05718e2e4d58251444cfd60f9b74
ceo-py <sfmf@abv.bg>
2023-01-31 16:42:09 +0200

    init

---
+++ Subqueries and Joins/17_highest_peak_and_longest_river_by_country.sql
@@ -0,0 +1,12 @@
+SELECT TOP(5) c.CountryName, MAX(p.Elevation) AS HighestPeakElevation, MAX(r.Length) AS LongestRiverLength
+FROM Countries c
+JOIN MountainsCountries AS mc
+ON c.CountryCode = mc.CountryCode
+JOIN Peaks AS p
+ON p.MountainId = mc.MountainId
+JOIN CountriesRivers AS cr
+ON cr.CountryCode = c.CountryCode
+JOIN Rivers AS r
+ON r.Id = cr.RiverId
+GROUP BY c.CountryName
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, c.CountryName ASC;
