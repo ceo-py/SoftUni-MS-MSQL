5b4528d3dab78e0cfacd19f39e2fbb07a006b2e8
ceo-py <sfmf@abv.bg>
2023-01-31 12:11:06 +0200

    init

---
+++ Subqueries and Joins/2_addresses_with_towns.sql
@@ -0,0 +1,7 @@
+SELECT TOP(50) FirstName, LastName, Name, AddressText
+FROM Employees AS e
+JOIN Addresses AS a
+ON e.AddressID = a.AddressId
+JOIN Towns AS t
+ON a.TownID = t.TownID
+ORDER BY FirstName, LastName
