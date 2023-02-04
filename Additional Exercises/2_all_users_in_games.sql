fe401c799eec8faad110e70c9dce84c0647c1863
ceo-py <sfmf@abv.bg>
2023-02-04 18:27:20 +0200

    init

--- Additional Exercises/2_all_users_in_games.sql
+++ Additional Exercises/2_all_users_in_games.sql
@@ -1,7 +1,11 @@
 SELECT g.Name AS Game, gt.Name AS Game_Type, u.Username, ug.Level, ug.Cash, c.Name AS Character
-FROM Games g
-JOIN GameTypes gt ON gt.Id = g.GameTypeId
-JOIN UsersGames ug ON ug.GameId = g.Id
-JOIN Users u ON u.Id = ug.UserId
-JOIN Characters c ON c.Id = ug.CharacterId
+FROM Games AS g
+JOIN GameTypes AS gt
+ON gt.Id = g.GameTypeId
+JOIN UsersGames AS ug
+ON ug.GameId = g.Id
+JOIN Users AS u
+ON u.Id = ug.UserId
+JOIN Characters AS c
+ON c.Id = ug.CharacterId
 ORDER BY Level DESC, Username, Game
