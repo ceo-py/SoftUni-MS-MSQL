8a310e7d794d435f37193d0a62d6a7847cd96061
ceo-py <sfmf@abv.bg>
2023-02-06 23:41:26 +0200

    init

---
+++ Databases Basics - Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/6_pilots_and_aircraft.sql
@@ -0,0 +1,8 @@
+SELECT p.FirstName, p.LastName, a.Manufacturer, a.Model, a.FlightHours
+FROM Pilots AS p
+JOIN PilotsAircraft AS pa
+ON p.Id = pa.PilotId
+JOIN Aircraft AS a
+ON pa.AircraftId = a.Id
+WHERE a.FlightHours IS NOT NULL and a.FlightHours <= 304
+ORDER BY a.FlightHours DESC, p.FirstName
