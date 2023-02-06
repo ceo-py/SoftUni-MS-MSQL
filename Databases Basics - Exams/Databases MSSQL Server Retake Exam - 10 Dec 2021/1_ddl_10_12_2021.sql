dde3f6475dc239b3181ac19e899cd0e2f76b1ab3
ceo-py <sfmf@abv.bg>
2023-02-06 22:11:25 +0200

    init

---
+++ Databases Basics - Exams/Databases MSSQL Server Retake Exam - 10 Dec 2021/1_ddl_10_12_2021.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Passengers (
+  Id INT PRIMARY KEY IDENTITY,
+  FullName VARCHAR(100) NOT NULL UNIQUE,
+  Email VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE Pilots (
+  Id INT PRIMARY KEY IDENTITY,
+  FirstName VARCHAR(30) NOT NULL UNIQUE,
+  LastName VARCHAR(30) NOT NULL UNIQUE,
+  Age TINYINT NOT NULL CHECK (Age >= 21 AND Age <= 62),
+  Rating FLOAT CHECK (Rating >= 0.0 AND Rating <= 10.0),
+);
+
+CREATE TABLE AircraftTypes (
+  Id INT PRIMARY KEY IDENTITY,
+  TypeName VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE Aircraft (
+  Id INT PRIMARY KEY IDENTITY,
+  Manufacturer VARCHAR(25) NOT NULL,
+  Model VARCHAR(30) NOT NULL,
+  Year INT NOT NULL,
+  FlightHours INT,
+  Condition CHAR(1) NOT NULL,
+  TypeId INT NOT NULL,
+  FOREIGN KEY (TypeId) REFERENCES AircraftTypes(Id),
+);
+
+CREATE TABLE PilotsAircraft (
+  AircraftId INT NOT NULL UNIQUE,
+  PilotId INT NOT NULL UNIQUE,
+  FOREIGN KEY (AircraftId) REFERENCES Aircraft(Id),
+  FOREIGN KEY (PilotId) REFERENCES Pilots(Id),
+  PRIMARY KEY (AircraftId, PilotId)
+);
+
+CREATE TABLE Airports (
+  Id INT PRIMARY KEY IDENTITY,
+  AirportName VARCHAR(70) NOT NULL UNIQUE,
+  Country VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE FlightDestinations (
+  Id INT PRIMARY KEY IDENTITY,
+  AirportId INT NOT NULL,
+  Start DateTime NOT NULL,
+  AircraftId INT NOT NULL,
+  PassengerId INT NOT NULL,
+  TicketPrice DECIMAL(18, 2) NOT NULL DEFAULT 15,
+  FOREIGN KEY (AirportId) REFERENCES Airports(Id),
+  FOREIGN KEY (AircraftId) REFERENCES Aircraft(Id),
+  FOREIGN KEY (PassengerId) REFERENCES Passengers(Id),
+);
+
