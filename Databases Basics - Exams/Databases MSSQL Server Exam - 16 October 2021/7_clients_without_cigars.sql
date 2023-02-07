c7c7b3f305de60b6cfccea0764b7d703fbf84d85
ceo-py <sfmf@abv.bg>
2023-02-07 14:19:33 +0200

    init

---
+++ Databases Basics - Exams/Databases MSSQL Server Exam - 16 October 2021/7_clients_without_cigars.sql
@@ -0,0 +1,16 @@
+SELECT c.Id, CONCAT(c.FirstName, ' ', c.LastName) as 'ClientName', c.Email
+FROM Clients AS c
+WHERE c.Id NOT IN (SELECT cc.ClientId FROM ClientsCigars AS cc)
+ORDER BY 'ClientName'
+
+
+-- SELECT c.Id,  CONCAT(c.FirstName, ' ', c.LastName) as 'ClientName', c.Email
+-- FROM Clients AS c
+-- LEFT JOIN ClientsCigars AS cc
+-- on c.Id = cc.ClientId
+-- WHERE c.Id NOT IN (SELECT cc.ClientId FROM ClientsCigars AS cc)
+-- ORDER BY 'ClientName'
+
+
+
+
