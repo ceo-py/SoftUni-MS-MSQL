6e35b4ced08dc8e4c8d3700914533fef505ca7b0
ceo-py <sfmf@abv.bg>
2023-02-07 23:51:47 +0200

    init

---
+++ Databases Basics - Exams/Databases MSSQL Server Exam - 13 February 2021/9_commits_in_repositories.sql
@@ -0,0 +1,8 @@
+SELECT TOP(5) r.Id, r.Name, COUNT(c.Id) as 'Commits'
+FROM Repositories AS r
+JOIN Commits AS c
+ON r.Id = c.RepositoryId
+JOIN RepositoriesContributors AS rc
+ON r.Id = rc.RepositoryId
+GROUP BY r.Id, r.Name
+ORDER BY 'Commits' DESC, r.Id, r.Name
