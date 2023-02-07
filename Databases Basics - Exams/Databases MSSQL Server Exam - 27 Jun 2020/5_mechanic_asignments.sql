fc37b18c95327ded59870ab83f1cbfaf5ed194d6
ceo-py <sfmf@abv.bg>
2023-02-07 22:10:02 +0200

    init

--- Databases Basics - Exams/Databases MSSQL Server Exam - 27 Jun 2020/5_mechanic_asignments.sql
+++ Databases Basics - Exams/Databases MSSQL Server Exam - 27 Jun 2020/5_mechanic_asignments.sql
@@ -1,4 +1,4 @@
-SELECT m.FirstName + ' ' + m.LastName AS Mechanic, Status, IssueDate
+SELECT m.FirstName + ' ' + m.LastName AS 'Mechanic', Status, 'I''ssueDate'
 FROM Mechanics AS m
 JOIN Jobs AS j ON m.MechanicId = j.MechanicId
 ORDER BY m.MechanicId, j.IssueDate, j.JobId
