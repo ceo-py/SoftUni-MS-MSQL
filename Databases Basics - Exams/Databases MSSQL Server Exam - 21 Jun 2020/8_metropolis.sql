05f0216d3a0c6c05adf87c457b9d70a0a77b9f22
ceo-py <sfmf@abv.bg>
2023-02-08 16:25:32 +0200

    init

--- Databases Basics - Exams/Databases MSSQL Server Exam - 21 Jun 2020/8_metropolis.sql
+++ Databases Basics - Exams/Databases MSSQL Server Exam - 21 Jun 2020/8_metropolis.sql
@@ -1,11 +1,6 @@
-SELECT a.Id AS 'AccountId', CONCAT(a.FirstName, ' ', a.LastName) AS 'FullName',
-       MAX(DATEDIFF(DAY, t.ArrivalDate, t.ReturnDate)) AS 'LongestTrip',
-       MIN(DATEDIFF(DAY, t.ArrivalDate, t.ReturnDate)) AS 'ShortestTrip'
-FROM Accounts AS a
-JOIN AccountsTrips AS at
-ON a.Id = at.AccountId
-JOIN Trips AS t
-ON at.TripId = t.Id
-WHERE a.MiddleName IS NULL AND t.CancelDate IS NULL
-GROUP BY a.Id, a.FirstName, a.LastName
-ORDER BY 'LongestTrip' DESC, 'ShortestTrip';
+SELECT TOP(10) c.Id, c.Name AS 'City', c.CountryCode AS 'Country', COUNT(a.CityId) AS 'Accounts'
+FROM Cities AS c
+JOIN Accounts AS a
+ON c.Id = a.CityId
+GROUP BY c.Id, c.Name, c.CountryCode
+ORDER BY 'Accounts' DESC
